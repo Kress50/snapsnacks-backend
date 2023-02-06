fbf4c94b3b89c742e4f5caf1cc74c1a06add7b41
Kress50 <kondalovi@gmail.com>
2023-02-06 20:45:43 +0400

    increased bcrypt saltrounds to 12

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -73,7 +73,7 @@ export class User extends CoreEntity {
   async hashPassword(): Promise<void> {
     if (this.password)
       try {
-        this.password = await bcrypt.hash(this.password, 10);
+        this.password = await bcrypt.hash(this.password, 12);
       } catch (e) {
         console.log(e);
         throw new InternalServerErrorException();
